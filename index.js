4fa309195ae18a1187143a8e078dd1c487cac41c
WAHU0508 <sharongikenye@gmail.com>
2024-06-29 07:14:30 +0300

    Declare and re-assign variables

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+//declare global variable using var
+var customerName = 'bob'
+
+//function to reassign variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+// console.log(customerName)
+// upperCaseCustomerName()
+// console.log(customerName)
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+// console.log(bestCustomer);
+// setBestCustomer();
+// console.log(bestCustomer);
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+// console.log(bestCustomer);
+// overwriteBestCustomer();
+// console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'milly';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'lisa';
+    return leastFavoriteCustomer
+}
+// console.log(leastFavoriteCustomer);
+// changeLeastFavoriteCustomer();
+// console.log(leastFavoriteCustomer);
